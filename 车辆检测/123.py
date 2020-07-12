d09bd191589ae5d32d32428dfc94b5eee0bb7b12
xinuonuo <837848987@qq.com>
2020-07-12 12:20:26 +0800

    车辆检测网页已实现

--- 车辆检测/123.py
+++ 车辆检测/123.py
@@ -1,28 +1,117 @@
 import urllib
 import base64
 import requests
+from flask import Flask,render_template
 
-host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=RSNlGSHlooTzKtfTdMglk0Sn&client_secret=rsjO33VAia5SrFK0KUEm2DiGL5su4vP0'
-response = requests.get(host)
-request = urllib.request.Request(host)
-request.add_header('Content-Type', 'application/json; charset=UTF-8')
-response = urllib.request.urlopen(request)
-content = response.read()
-if content:
-    print(type(content))
-content_str=str(content, encoding="utf-8")
-###eval将字符串转换成字典
-content_dir = eval(content_str)
-access_token = content_dir['access_token']
+from aip import AipImageClassify
+import tkinter as tk
+from tkinter import filedialog
+import os
+import cv2
+import matplotlib.pyplot as plt
+import io
+import base64
 
-request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/vehicle_detect"
-# 二进制方式打开图片文件
-f = open('car1.jpg', 'rb')
-img = base64.b64encode(f.read())
+"""
+  APPID AK SK
+"""
+# 在百度云创的实例应用 获取的三个参数填写到下面
 
-params = {"image": img}
-request_url = request_url + "?access_token=" + access_token
-headers = {'content-type': 'application/x-www-form-urlencoded'}
-response = requests.post(request_url, data=params, headers=headers)
-if response:
-    print(response.json())
+def get_file_content(file_path):
+    with open(file_path, 'rb') as f:
+        return f.read()
+
+def get_img_stream(img_local_path):
+  """
+  工具函数:
+  获取本地图片流
+  :param img_local_path:文件单张图片的本地绝对路径
+  :return: 图片流
+  """
+  img_stream = ''
+  with open(img_local_path, 'rb') as img_f:
+    img_stream = img_f.read()
+    img_stream = base64.b64encode(img_stream).decode()
+  return img_stream
+
+img_path = None
+img_url = None
+
+
+app = Flask(__name__)
+
+"""
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+"""
+
+@app.route("/")
+def definCar():
+    return render_template("vehicleDetect.html")
+
+# 打开图片文件并读取二进制图片信息
+@app.route("/defineCar")
+def define():
+    if len(img_path)>0:
+        f= get_file_content(img_path)
+        host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=RSNlGSHlooTzKtfTdMglk0Sn&client_secret=rsjO33VAia5SrFK0KUEm2DiGL5su4vP0'
+        response = requests.get(host)
+        request = urllib.request.Request(host)
+        request.add_header('Content-Type', 'application/json; charset=UTF-8')
+        response = urllib.request.urlopen(request)
+        content = response.read()
+        if content:
+            print(type(content))
+        content_str = str(content, encoding="utf-8")
+        ###eval将字符串转换成字典
+        content_dir = eval(content_str)
+        access_token = content_dir['access_token']
+
+        request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/vehicle_detect"
+        # 二进制方式打开图片文件
+        f = open(img_path, 'rb')
+        img = base64.b64encode(f.read())
+        params = {"image": img}
+        request_url = request_url + "?access_token=" + access_token
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        response = requests.post(request_url, data=params, headers=headers)
+        if response:
+            print(response.json())
+        car_message= response.json()
+        return  render_template("vehicleDetect.html",message = car_message,picture_url = img_url)
+    else:
+        return render_template("vehicleDetect.html",message = "无图片，请选择一张图片进行识别")
+
+
+@app.route("/selectPicture")
+def openPicture():
+    application_window = tk.Tk()
+    application_window.withdraw()  # 将创建的tk窗口隐藏
+
+    # 设置文件对话框会显示的文件类型txt files (*.txt)|*.txt|All files (*.*)|*.*
+    my_filetypes = [('all files', '.*'), ('text files', '.txt')]
+
+    # 打开一个文件选择对话框，选择汽车图片
+    filename = filedialog.askopenfilename(parent=application_window,
+                                        initialdir=os.getcwd(),
+                                        title="请选择一张汽车图片",
+                                        filetypes=my_filetypes)
+    print(filename)
+    if len(filename)>0:
+        #获得用户选择的图片存储在服务器上的地址
+        global img_url
+        img_url = get_img_stream(filename)
+        global img_path
+        img_path = filename
+        return render_template("vehicleDetect.html",picture_url = img_url)
+    else:
+        return render_template("vehicleDetect.html")
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
